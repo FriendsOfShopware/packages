e64a4522309da32e4478e010575ba05b943bbe46
Soner Sayakci <s.sayakci@shopware.com>
2020-09-03 12:05:29 +0200

    Remove unneeded fields

--- src/Struct/Shop/ShopwareVersion.php
+++ src/Struct/Shop/ShopwareVersion.php
@@ -7,19 +7,4 @@ class ShopwareVersion extends \App\Struct\Struct
     public int $id;
 
     public string $name;
-
-    public int $parent;
-
-    public bool $selectable;
-
-    public string $major;
-
-    /**
-     * @var null
-     */
-    public $releaseDate;
-
-    public bool $public;
-
-    public static $mappedFields = [];
 }
