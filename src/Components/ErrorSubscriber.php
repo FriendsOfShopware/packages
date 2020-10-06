6ee026db3ef5c596b7d002e05dce62413d3f70c8
Soner Sayakci <s.sayakci@shopware.com>
2020-10-06 17:57:16 +0200

    Show report window when errors occurs, fixes #83

--- src/Components/ErrorSubscriber.php
+++ src/Components/ErrorSubscriber.php
@@ -5,17 +5,27 @@ declare(strict_types=1);
 namespace App\Components;
 
 use App\Components\Api\Exceptions\AccessDeniedException;
+use Sentry\State\Hub;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Core\Exception\InsufficientAuthenticationException;
+use Twig\Environment;
 
 class ErrorSubscriber implements EventSubscriberInterface
 {
+    private Environment $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -48,5 +58,16 @@ class ErrorSubscriber implements EventSubscriberInterface
                 )
             );
         }
+
+        $lastEvent = Hub::getCurrent()->getLastEventId();
+
+        $response = new Response();
+        $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        $tpl = $this->twig->render('error.html.twig', [
+            'lastEventId' => $lastEvent
+        ]);
+        $response->setContent($tpl);
+
+        $event->setResponse($response);
     }
 }
